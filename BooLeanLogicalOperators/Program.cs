b6be849448a164f0d35960a9513912efff4aeef9
Opilane TTHK <opilane@tthk.ee>
2021-09-14 12:03:14 +0300

    First Commit

---
+++ BooLeanLogicalOperators/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace BooLeanLogicalOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja kasutajatunnust
+            //programm küsib kasutaja parooli
+            //programm kontrollib kas mõlemad on õiged
+            //kui mõlemad on õiged konsool be like Tere Tulemast
+            //Kui on vale siis programm be like sisselogimine ebaõnnestus
+            //kt:admin pr:admin1234
+
+
+            Console.WriteLine("Kasutajanimi");
+            string Kasutaja = (Console.ReadLine());
+            Console.WriteLine("Parool");
+            string Parool = (Console.ReadLine());
+            if (Kasutaja == "admin" && Parool == "admin1234") 
+            {
+                Console.WriteLine("Tere Tulemast!");
+            }
+            else
+            {
+                Console.WriteLine("Lahku");
+            }
+               
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
